ee876ed5261d36940e774be2d9604be5c746af50
Aleksandr <a.gerasimenko.yuz@gmail.com>
2022-09-02 22:36:50 +0300

    EnterToApp

--- Header.h
+++ Header.h
@@ -1,1 +1,183 @@
 #pragma once
+using namespace std;
+
+struct IRegistration
+{
+	virtual void ICreatAcount() = 0;
+	virtual int IEnterToApp() = 0;
+
+};
+class human 
+{
+protected:
+	string name;
+	string surname;
+	string login;
+	string password;
+	int ID;
+
+public:
+	human() :name{}, surname{}, login{}, password{}, ID{ 0 }{}
+	human(const string& nameP, const string& surnameP, const string& loginP, const string& passwordP, int IDP ) : name{ nameP }, surname{ surnameP }, login{ loginP },
+		password{ passwordP },  ID{IDP}{}
+	virtual void Show() = 0;
+	virtual void edit() = 0;
+	string GetName()
+	{
+		return name;
+	}
+	string GetSurname()
+	{
+		return surname;
+	}
+	string GetLgin()
+	{
+		return login;
+	}
+	string GetPassword()
+	{
+		return 	password;
+	}
+	int GetID()
+	{
+		return 	ID;
+	}
+	void SetName()
+	{
+		cout << "Enter your name ";
+		cin>> name;
+	}
+	void SetSurname()
+	{
+		cout << "Enter your surname ";
+		cin>> surname;
+	}
+	void SetLgin()
+	{
+		cout << "Enter login ";
+		cin>> login;
+	}
+	void SetPassword()
+	{
+		cout << "Enter password ";
+		cin	>> password;
+	}
+	void SetID()
+	{
+		srand(time(NULL));
+		ID = rand() % 1000 + 1001;
+	}
+	/*void ²²dentification()
+	{
+		cout << " Identification()" << endl;
+	}*/
+
+};
+
+class student : public human, public IRegistration
+{
+	string address;
+	string phone_number;
+public:
+	student() :human(), address{}, phone_number{}{}
+	student(const string& nameP, const string& surnameP, const string& loginP, const string& passwordP, int IDP , const string& addressP, const string& phone_numberP) :
+		human(nameP, surnameP, loginP, passwordP, IDP), address{ addressP }, phone_number{ phone_numberP }{}
+
+	void Show()
+	{
+		cout << "Student's name: " << name << endl;
+		cout << "surname:        " << surname << endl;
+		cout << "Login:           " << login << endl;
+		cout << "Passwor:        " << password << endl;
+		cout << "Address:        " << address << endl;
+		cout << "Phone number:   " << phone_number << endl;
+		cout << "ID:             " << ID << endl;
+		cout << " ================================================== " << endl;
+	}
+
+	void SetStudent()
+	{
+		
+		cout << "Dear student! We are glad to see you here!!!" << endl;
+		
+		SetName();
+		SetSurname();
+	    SetLgin();
+		SetPassword();
+		SetID();
+		SetPhone();
+		SetAddress();
+	}
+	void SetAddress()
+	{
+		cout << "Enter address ";
+		cin >> address;
+	}
+	void SetPhone()
+	{
+		cout << "Enter phone number ";
+		cin >> phone_number;
+	}
+	string GetAddress()
+	{
+		return address;
+	}
+	string GetPhone_number()
+	{
+		return phone_number;
+	}
+	void edit()
+	{
+		cout << "good";
+	}
+	void edit2()
+	{
+		cout << "666666666666666666666666666666666666666666666";
+	}
+	
+	void ICreatAcount()
+	{
+		cout << "student IReg()" << endl;
+	}
+	int IEnterToApp()
+	{
+		cout << "student Identification()" << endl;
+		return 1;
+	}
+};
+class Admin : public human, public IRegistration
+{
+
+public:
+	Admin(const string& nameP, const string& surnameP, const string& loginP, const string& passwordP, int IDP) : human(nameP, surnameP, loginP, passwordP, IDP) {}
+
+
+	void Show()
+	{
+		cout << "Admin ==============!" << endl;
+		cout << name << endl;
+		cout << surname << endl;
+		cout << login << endl;
+		cout << password << endl;
+
+
+	}
+	void edit()
+	{
+		cout << "good";
+	}
+	void edit1()
+	{
+		cout << "55555555555555555555555555555555555555555555555555";
+	}
+	void ICreatAcount()
+	{
+		cout << "aD IReg()" << endl;
+	}
+	int IEnterToApp()
+	{
+		cout << "ad Identification()" << endl;
+		return 1;
+	}
+	
+};
--- KnowledgeTestingSystem.cpp
+++ KnowledgeTestingSystem.cpp
@@ -1,102 +1,239 @@
 #include <iostream>
 #include <windows.h>
-#include <io.h>
+#include <fstream>
 #include <stdio.h>
+#include "Header.h"
+#include <vector>
 using namespace std;
 
-struct IRegistration
+class ListOFPeople : public IRegistration
 {
-	virtual void Registration() = 0;
-	virtual void ²²dentification() = 0;
-
-};
-
-
-class human : public IRegistration
-{
-protected:
-	string name;
-	string surname;
-	string login;
-	string password;
-
+	vector <student> Init;
 public:
-	human() :name{}, surname{}, login{}, password{}{}
-	human(const string& nameP, const string& surnameP) : name{ nameP }, surname{ surnameP }{}
-	virtual void Show() = 0;
-	virtual void edit() = 0;
-	void Registration()
+	void Add(student& obj)
 	{
-		cout << "IReg()" << endl;
+		Init.push_back(obj);
+	
 	}
-	void ²²dentification()
+	void ShowStudent()
 	{
-		cout << " Identification()" << endl;
+		vector <student>  ::iterator iter = Init.begin();
+		for (; iter != Init.end(); iter++)
+		{
+			iter->Show();
+			}
+	}
+	bool HaveLogin(const string& login)
+	{
+		vector <student>  ::iterator iter = Init.begin();
+		for (; iter != Init.end(); iter++)
+		{
+			if (login == iter->GetLgin())
+			{
+				return true;
+			}
+
+		}
+		return false;
+	}
+	bool HavePassword(const string& password)
+	{
+		vector <student>  ::iterator iter = Init.begin();
+		for (; iter != Init.end(); iter++)
+		{
+			if (password == iter->GetPassword())
+			{
+				return true;
+			}
+
+		}
+		return false;
+	}
+	void Logo()
+	{
+		system("cls");
+		cout << "                  ___________________________" << endl;
+		cout << "                 ^ Knolwlege Testing Systeam ^                      " << endl;
+		cout << "                 ^___________________________^" << endl << endl;
+	}
+	void Enter()
+	{
+		Logo();
+		ReadFile();
+		cout << "Enter -1- if you have an acount" << endl;
+		cout <<"Enter -2- if you want to create an acount" << endl;
+		//system("cls");
+		int b;
+		cin >> b;
+		system("cls");
+		switch (b)
+		{
+		case(1):
+		{
+			int a=IEnterToApp();
+			if (a > 3)
+				system("cls");
+			{
+				Enter();
+			}
+			break;
+		}
+		case(2):
+		{
+			ICreatAcount();
+			break;
+		}
+		default:
+		{
+			Enter();
+		}
+		}
+
+			
+
+	}
+	int IEnterToApp()
+	{
+		Logo();
+		string a;
+		string b;
+		cout << "<<<<<<<<<<<<<<<<<<<<<<  HELLO STUDENT  >>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl; 
+		cout << "                 Enter your username to login" << endl;
+		cout << "_______: " ;
+		cin >>a;
+		cout << "                Enter your username to Password" << endl;
+		cout << "_______: " ;
+		cin >> b;
+		int t = 0;
+		while (!HaveLogin(a)||!HavePassword(b))
+		{
+			if (t > 3)
+				break;
+			system("cls");
+			//cout << "LOGIN --> " << a << endl;
+			cout << "Login or password entered incorrectly. REPEAT!!! " << endl;
+			cout << "   Enter your username to login" << endl;
+			cin >> a;
+			cout << "   Enter your username to Password" << endl;
+			cin >> b;
+			t++;
+		}
+		return t;
+
+	}
+	void ICreatAcount()
+	{
+				student temp;
+			temp.SetStudent();
+			cout << endl << endl;
+		 while (HaveLogin(temp.GetLgin()))
+		 {
+			 system("cls");
+		     cout << "LOGIN --> " << temp.GetLgin() << endl;
+			 cout << "THIS LOGIN IS BEING USED. You need to enter another login! " << endl;
+			 temp.SetLgin();
+			
+		 }
+		 cout << endl;
+		temp.Show();
+		int b;
+		cout << endl;
+		cout << "If everything is correct, press 5 to create an account!!!" << endl;
+		cin >> b;
+		if (b == 5)
+		{
+			Init.push_back(temp);
+			WriteFile();
+			system("cls");
+			cout << "Congratulations! YOU HAVE ACCOUNT!!!" << endl;
+		}
+		else
+		{
+			ICreatAcount();
+		}
+	}
+	
+	void WriteFile()
+	{
+		string a("DataPeople.txt");
+		fstream fs;
+		fs.open(a,  fstream::out);
+		if (!fs.is_open())
+		{
+			cout << "Error open file" << endl; //Error open file
+		}
+		vector <student>  ::iterator iter = Init.begin();
+		for (; iter != Init.end(); iter++)
+		{
+			//fstream fs;
+			fs << iter->GetName() << "\n";
+			fs << iter->GetSurname() << "\n";
+			fs << iter->GetLgin() << "\n";
+			fs << iter->GetPassword() << "\n";
+			fs << iter->GetAddress() << "\n";
+			fs << iter->GetPhone_number() << "\n";
+			fs << iter->GetID() << "\n";
+
+		}
+		fs.close();
+	}
+	void ReadFile()
+	{
+		string a("DataPeople.txt");
+		fstream fs;
+		fs.open(a, fstream::in );
+		if (!fs.is_open())
+		{
+			cout << "Your account will be the first" << endl; //Error open file
+		}
+		else
+		{
+			//student temp;
+			string name;
+			string surname;
+			string Password;
+			string login;
+			string address;
+			string phone_nomber;
+			int ID;
+			int b = 0;
+			while (!fs.eof())
+			{
+			
+				fs >> name;
+				fs >> surname;
+				fs >> login;
+				fs >> Password;
+				fs >> address;
+				fs >> phone_nomber;
+				fs >> ID;
+				ID = 100;
+				student temp(name, surname, login, Password, ID, address, phone_nomber);
+				Init.push_back(temp);
+				//b++;
+			}
+
+			Init.pop_back();
+			fs.close();
+		}
 	}
 
 };
-
-class student : public human
-{
-	string address;
-	string phone_number;
-public:
-	student() :human(), address{}, phone_number{}{}
-	student(const string& addressP, const string& phone_numberP, const string& nameP, const string& surnameP) : human(nameP, surnameP), address{ addressP }, phone_number{ phone_numberP }{}
-
-	void Show()
-	{
-		cout << "Student ==============!" << endl;
-		cout << name << endl;
-		cout << surname << endl;
-		cout << login << endl;
-		cout << password << endl;
-		cout << address << endl;
-		cout << phone_number << endl;
-
-	}
-	void edit()
-	{
-		cout << "good";
-	}
-
-};
-class Admin : public human
-{
-
-public:
-	Admin(const string& nameP, const string& surnameP) : human(nameP, surnameP) {}
-
-
-	void Show()
-	{
-		cout << "Admin ==============!" << endl;
-		cout << name << endl;
-		cout << surname << endl;
-		cout << login << endl;
-		cout << password << endl;
-
-
-	}
-	void edit()
-	{
-		cout << "good";
-	}
-
-};
-
-
-
+ 
 
 
 int main()
 {
+	//student temp(name, surname, Password, login, ID, address, phone_nomber);
+	//Init.push_back( student r("Ivan", "Bobrov", "email", "33333", 1111, "0937018395", "Stroiteley"));
+	
+	ListOFPeople T;
 
-	human* test = new student("qwsd", "qsdsw", "sdqw", "qwsd");
-	// test = new teacher("qwsd", "qsdsw");
-	test->Show();
-	test->Registration();
-	test->²²dentification();
+	T.Enter();
+	system("cls");
+	cout << "CONGTETULATE!CONGTETULATECONGTETULATE!CONGTETULATE!CONGTETULATE!CONGTETULATE!CONGTETULATE!CONGTETULATE!CONGTETULATE!ATE!CONGTETULATE!"<<endl;
+
+	
 
 	return 0;
 }
